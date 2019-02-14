90c45448adb5e330aea59e9e67b366d9a285d43b
ffrancajordao <42717180+ffrancajordao@users.noreply.github.com>
2019-02-14 16:34:04 -0500

    Adding Organizations

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
 //Node.js app requirements. These lines only work if you've already installed them.
 var express             = require("express"),
     parse               = require("body-parser"),
@@ -6,36 +11,79 @@ var express             = require("express"),
     redirectToHTTPS     = require("express-http-to-https").redirectToHTTPS,
     app                 = express();
 
+
 //This just makes it so that we don't have to type out '.ejs' after every webpage route
-//note: .ejs files stand for express js
+//note: .ejs files stand for embedded javascript
 app.set("view engine", "ejs");
 
 //This is for images, scripts, and stylesheets in the public directory so we don't
 //have to type the public directory when calling js and css files in our htlm headers
 app.use(express.static(__dirname + "/public/"));
 
-//Can't remember what this does but it's 100% necessary
+//This allows us to extract post request data and use it server-side
 app.use(parse.urlencoded({extended: true}));
 
 //HTTP is default route (we don't want this). This redirects all routes to HTTPS for security
 app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
 
-// var con = mysql.createConnection({
-//   host: "localhost",
-//   user: "yourusername",
-//   password: "yourpassword"
-// });
+var con = db.createConnection({
+    host     : 'db-segfault-cap.cae0l6rwojdw.us-east-1.rds.amazonaws.com',
+    port     : '3306',
+    user     : 'Segfaultcapstone',
+    password : 'S3gfault2019',
+    database : 'db-segfault-cap'
+});
 
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-// });
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to MySQL database.");
+});
 
 app.get("/", function(req, res){
     res.render("home");
 });
 
-//Server initiation
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Server initiated (port " + process.env.PORT + ")...");
+app.get("/devices", function(req, res){
+    con.query("SELECT * FROM Devices", function (err, result, fields) {
+        if (err) throw err;
+        res.render("devices", {devices: result});
+    });
 });
+
+app.get("/organizations", function(req, res){
+    con.query("SELECT * FROM Organizations", function (err, result, fields) {
+        if (err) throw err;
+        res.render("organizations", {organizations: result});
+    });
+});
+
+app.post("/devices", function(req, res){
+    var sql = "INSERT INTO Devices (dev_Name, dev_SN, dev_Description, dev_Active) VALUES ?";
+    var devices = [
+        [req.body.dev_Name, req.body.dev_SN, req.body.dev_Description, 'true']
+    ];
+    con.query(sql, [devices], function(err, result){
+        if(err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+    });
+    res.redirect("devices");
+});
+
+app.post("/organizations", function(req, res){
+    var sql = "INSERT INTO Organizations (org_OrgName, org_BudgetCode) VALUES ?";
+    var organizations = [
+        [req.body.org_OrgName, req.body.org_BudgetCode]
+    ];
+    con.query(sql, [organizations], function(err, result){
+        if(err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+    });
+    res.redirect("organizations");
+});
+
+
+//Server initiation
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
