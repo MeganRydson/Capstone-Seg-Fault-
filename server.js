3c5a2ca3c86fd68b2dd31da3dcfc1bbeb7f68acb
chloenb <cbergeron@rollins.edu>
2019-02-14 20:23:45 +0000

    Chloe's Push

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //Node.js app requirements. These lines only work if you've already installed them.
 var express             = require("express"),
-    parse               = require("body-parser"),
+    parse               =  require("body-parser"),
     db                  = require("mysql"),
     mailer              = require("nodemailer"),
     redirectToHTTPS     = require("express-http-to-https").redirectToHTTPS,
@@ -32,8 +32,14 @@ app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
 // });
 
 app.get("/", function(req, res){
-    res.render("home");
+    var devices = [
+        {dev_ID: 001, dev_Name: "Dev1", dev_SN: "12345", dev_Description: "The first device", dev_Available: true},
+        {dev_ID: 002, dev_Name: "Dev2", dev_SN: "23456", dev_Description: "I'm second!", dev_Available: false},
+        {dev_ID: 003, dev_Name: "Dev3", dev_SN: "34537", dev_Description: "Last but not least", dev_Available: true}
+    ]
+    res.render("home", {devices: devices});
 });
+
 
 //Server initiation
 app.listen(process.env.PORT, process.env.IP, function(){
