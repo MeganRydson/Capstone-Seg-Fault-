f53882ee437002b39132588e099d75dbc8cdc65b
chloenb <cbergeron@rollins.edu>
2019-03-21 17:42:55 +0000

    Calendar

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,11 @@
-$(document).ready(function(){
-    $('.ui.checkbox').checkbox();
-});
-
-$('.modal-pop[data-modal]').click(function() {
-    $('.modal[data-modal=' + $(this).attr('data-modal') + ']').modal().modal('show');
-});
+$(document).ready(function() {
+  $(".ui.checkbox").checkbox();
+  $(".modal-pop[data-modal]").click(function() {
+    $(".modal[data-modal=" + $(this).attr("data-modal") + "]")
+      .modal()
+      .modal("show");
+  });
+  $("#calendar").fullCalendar({
+    defaultView: "month",
+  });
+ });
