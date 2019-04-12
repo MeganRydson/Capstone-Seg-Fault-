68c284c405bf360a3d232767d5c26a864d1e02b2
Megan Rydson <mrydson@rollins.edu>
2019-04-12 15:51:15 +0000

    WORKING AUTH WOOHOO

--- routes/index.js
+++ routes/index.js
@@ -30,7 +30,7 @@ router.post("/register", function(req, res){
            return res.render('register');
        } 
        passport.authenticate("local")(req, res, function(){
-          res.redirect("./secret");
+          res.redirect("/");
        });
     });
 });
