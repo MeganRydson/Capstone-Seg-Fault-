81bc19fe58409aca0aa635d937a93e8b75681437
Brian <bchren@rollins.edu>
2019-04-26 01:38:27 +0000

    4/25 added styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     $(".ui.checkbox").checkbox();
     $('.remove-button[data-modal]').click(function() {
-        $('.modal[data-modal=' + $(this).attr('data-modal') + ']').modal().modal('show');
+        $('.modal[data-modal=' + $(this).attr('data-modal') + ']')
+        .modal().modal('show');
     });
     $('#batch-delete').click(function() {
         $('.modal').modal('show');
@@ -20,53 +21,28 @@ $(document).ready(function() {
       }),
     });
     };
-  //   document.addEventListener('DOMContentLoaded', function() {
-  //   var calendarEl = document.getElementById('calendar');
-  
-  //   var calendar = new FullCalendar.Calendar(calendarEl, {
-  //     plugins: [ 'interaction', 'dayGrid', 'timeGrid' ],
-  //     timeZone: 'UTC',
-  //     defaultView: 'dayGridMonth',
-  //     header: {
-  //       left: 'prev,next today',
-  //       center: 'title',
-  //       right: 'dayGridMonth,timeGridWeek,timeGridDay'
-  //     },
-  //     editable: true,
-  
-  //     // JSON FEED INSTRUCTIONS
-  //     //
-  //     // 1. Open a new browser tab. Go to codepen.io
-  //     //
-  //     // 2. Create a new pen (Create > New Pen)
-  //     //
-  //     // 3. Paste your JSON into the JS pane
-  //     //
-  //     // 4. Hit the "Save" button
-  //     //
-  //     // 5. The page's URL will change. It will look like this:
-  //     //    https://codepen.io/anon/pen/eWPBOx
-  //     //
-  //     // 6. Append ".js" to it. Will become like this:
-  //     //    https://codepen.io/anon/pen/eWPBOx.js
-  //     //
-  //     // 7. Paste this URL below.
-  //     //
-  //     events: events.map(function (event) {
-  //       return {
-  //         title: event.ev_Name,
-  //         start: event.ev_StartDate,
-  //         end: event.ev_EndDate,
-  //         allDay : false,
-  //         url: "/event-" + event.ev_ID
-  //       }
-  //     })
-  
-  //     // 8. Then, enter a date for defaultDate that best displays your events.
-  //     //
-  //     // defaultDate: 'XXXX-XX-XX'
-  //   });
-  
-  //   calendar.render();
-  // });
+    
+    $("[href]").each(function() {
+      if (this.href == window.location.href) {
+        if($(this).attr("id") == "spec-menu-item") {
+          $(this).addClass("spec-active");
+        } else {
+          $(this).addClass("active");
+        }
+      }
+    });
+    
  });
+ 
+//----------------------------------------------
+// NAVBAR MENU ACTIVE ITEM
+
+$(".menu a").click(function() {
+    $(".menu a").removeClass("active");
+    $(this).addClass("active");
+})
+
+$("#spec-menu-item").click(function() {
+    $(".menu a").removeClass("active");
+    $(this).addClass("spec-active");
+})
